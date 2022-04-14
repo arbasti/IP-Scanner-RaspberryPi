10eb871d6cca4059988170f4f32b37dcd0cfefa6
arbasti <90555409+arbasti@users.noreply.github.com>
2022-04-14 20:33:41 +0200

    Update python_ip_scanner.py

--- python_ip_scanner.py
+++ python_ip_scanner.py
@@ -1,15 +1,31 @@
+"""
+MVP :
+Récupérer les adresses IP distribué avec un code en python
+
+Description :
+La méthode est par Brutforce
+Le problème étant que cette méthode est très longue en attente
+
+Sources :
+- https://tutoriels.pecaudchristopher.com/tutoriels_windows/espace_python/Tutoriel_Creation_Scanner_IP_Python.php#Intro
+- https://docs.python.org/3/library/socket.html
+"""
+
 import socket
 
-for ping in range(1,254):
-    address = "192.168.1." + str(ping)  # On écrit toutes les adresses ip possibles
+# On ping les adresses IP disponibles
+for ping in range(1,254):  # 0 et 255 étant le routeur et le broadcast ne sont pas compris
+    address = "192.168.1." + str(ping)  # On écrit toutes les adresses IP de 1 à 254
     socket.setdefaulttimeout(1)
 
     try:
+        # On ping les adresses IP une par une et on renvoie un triple dans les variables
         hostname, alias, addresslist = socket.gethostbyaddr(address)
-
+    
+    # Si on ne reçoit rien en retour après le ping, on implémente None au différente variable
     except socket.herror:
         hostname = None
         alias = None
         addresslist = address
 
-    print(addresslist, '=>', hostname)
+    print(addresslist, '=>', hostname)  # On print le résultat de notre recherche
